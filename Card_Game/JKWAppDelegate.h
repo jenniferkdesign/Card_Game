0735f21e66d9f7be451044c387fa3e2abb17541c
Jennifer K Waldner <jenniferkwaldner@gmail.com>
2014-06-29 19:05:58 -0400

    Initial Commit

---
+++ Card_Game/JKWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKWAppDelegate.h
+//  Card_Game
+//
+//  Created by Jennifer Waldner on 6/29/14.
+//  Copyright (c) 2014 Jennifer K Design, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
